3f78d284cef9e43e49bbd19d48d1a32633a6027f
David OK <david.ok8@gmail.com>
2017-03-08 23:29:26 +0000

    MAINT: fix config file for GitHub pages.

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,7 +32,7 @@ AUTHOR_FEED_RSS = None
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
 
 
 PLUGIN_PATHS = ['./plugins']
